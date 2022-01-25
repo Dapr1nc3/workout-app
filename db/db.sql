3fcad1a1f38d51694ae9d153e9c1143bbaa6d8a0
dapr1nc3 <ddaprincem84@gmail.com>
2022-01-25 15:48:48 -0500

    added new sql files, populated the new tables, user and measurements

--- db/db.sql
+++ db/db.sql
@@ -1,3 +1,4 @@
 DROP DATABASE IF EXISTS git_fit_db;
 
 CREATE DATABASE git_fit_db;
+USE git_fit_db;
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO user (username, email, password)
+VALUES
+    ('Ronald Mcdonald', 'RonaldMcc@gmail.com', 'Mcchicken112!'),
+    ('King Aruthor', 'KingA@gmail.com', 'BigGuywithasword222');
+
+INSERT INTO measurements (height, lbs, chest, waist, biceps, hips, calfs, thighs)
+VALUES
+    ('5 feet 9 inches', '150lbs', '41 inches', '32 inches', '13 inches', '30 inches', '14 inches', '22 inches');
