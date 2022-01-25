fc6ecf4af9557b62bc36a7fb2e13a4d202dd0765
Alex Betancourt <alex@Alexs-MacBook-Pro.local>
2022-01-25 14:41:05 -0500

    Created first route to landing page (pg1), added middleware to access public folder, imported html routes and successfully tested first route

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const sequelize = require("./config/connection");
 
+const htmlRoutes = require('./routes/htmlRoutes');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// express middleware
+// express middleware 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// routes
+app.use('/', htmlRoutes);
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
