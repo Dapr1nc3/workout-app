9f7a73cf3d8e318e9d3a720efb27fd79d545ee60
Alex Betancourt <alex@Alexs-MacBook-Pro.local>
2022-01-25 15:49:21 -0500

    added comment to connection.js file

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PW,
   {
-    // original: host: 'localhost'
+    // original: host: 'localhost. if you have any issues with localhost, use "127.0.0.1'
     host: "127.0.0.1",
     dialect: "mysql",
     port: 3306,
