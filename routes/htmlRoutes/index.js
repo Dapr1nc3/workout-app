45dc40c60c822227b5298518b714c37bd48ef37f
Alex Betancourt <alex@Alexs-MacBook-Pro.local>
2022-01-25 14:48:27 -0500

    added remaining html routes and tested pg2-4 successfully

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,9 +1,25 @@
 const router = require('express').Router();
 const path = require('path');
 
+// route for landing page 
 router.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '../.././public/page1.html'));
-})
+});
+
+// route for page 2
+router.get('/page2', (req, res) => {
+    res.sendFile(path.join(__dirname, '../.././public/page2.html'));
+});
+
+// route for page 3 
+router.get('/page3', (req,res) => {
+    res.sendFile(path.join(__dirname, '../.././public/page3.html'));
+});
+
+// route for page 4
+router.get('/page4', (req, res) => {
+    res.sendFile(path.join(__dirname, '../.././public/page4.html'));
+});
 
 module.exports = router
  
