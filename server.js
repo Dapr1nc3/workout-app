713c35b52f2d09f7c377c9b59c6c93a8451c291e
dapr1nc3 <ddaprincem84@gmail.com>
2022-01-30 18:13:39 -0500

    changed file structure & names added routes to access

--- server.js
+++ server.js
@@ -1,18 +1,30 @@
 const express = require("express");
 const sequelize = require("./config/connection");
-
-const htmlRoutes = require('./routes/htmlRoutes');
+const path = require('path');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const routes = require('./controller')
+const sess = {
+  secret: "secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // express middleware 
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 // routes
-app.use('/', htmlRoutes);
+app.use(routes)
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
