39a40cb55ec9bf13ce640a5f8ff96de5882a97b7
camyacodes <camyacodes@gmail.com>
2022-01-29 20:59:45 -0500

    updated carousel with target muscle and name

--- public/assets/js/page3.js
+++ public/assets/js/page3.js
@@ -25,10 +25,66 @@ var selectTargetMuscle = function (data) {
 
     var targetMuscle = "abs"
 
-	for (let i = 0; i < 3; i++) {
+    if (data[0].target === targetMuscle) {
+        var carouselContainerEL = document.querySelector("#exercise_carousel_container")
+
+        var exerciseCardContainer = document.createElement("div")
+
+        exerciseCardContainer.classList = "carousel-item active"
+
+        carouselContainerEL.appendChild(exerciseCardContainer)
+
+        var nameEl = document.createElement("h1")
+
+        nameEl.classList = "text-center"
+
+        nameEl.textContent = data[0].name
+
+        exerciseCardContainer.appendChild(nameEl)
+
+        var gifUrl = data[0].gifUrl;
+
+        var demoGifEl = document.createElement("img");
+        
+        demoGifEl.classList = "rounded mx-auto d-block p-5"
+
+        demoGifEl.setAttribute("src", gifUrl);
+        
+        exerciseCardContainer.appendChild(demoGifEl)
+    }
+
+    for (let i = 1; i < 5; i++) {
+        
+
 
 		if (data[i].target === targetMuscle) {
-			console.log(data[i]);
+			// console.log(data[i]);
+
+            var carouselContainerEL = document.querySelector("#exercise_carousel_container")
+
+            var exerciseCardContainer = document.createElement("div")
+
+            exerciseCardContainer.classList = "carousel-item"
+
+            carouselContainerEL.appendChild(exerciseCardContainer)
+
+            var nameEl = document.createElement("h1")
+
+            nameEl.classList = "text-center"
+
+            nameEl.textContent = data[i].name
+
+            exerciseCardContainer.appendChild(nameEl)
+
+            var gifUrl = data[i].gifUrl;
+
+            var demoGifEl = document.createElement("img");
+            
+            demoGifEl.classList = "rounded mx-auto d-block p-5"
+
+            demoGifEl.setAttribute("src", gifUrl);
+            
+            exerciseCardContainer.appendChild(demoGifEl)
 
 		}
 
