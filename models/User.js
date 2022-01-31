d327882590a56cc267c1c4659df4c8b4452a6fd1
chaudeveloper <ms_michanguyen@yahoo.com>
2022-01-31 15:03:12 -0500

    login fully functional to validate username/password, fixing past commit

--- models/User.js
+++ models/User.js
@@ -5,9 +5,9 @@ const bcrypt = require('bcrypt');
 // create our User model
 class User extends Model {
   // set up method to run on instance data (per user) to check password
-//   checkPassword(loginPw) {
-//     return bcrypt.compareSync(loginPw, this.password);
-//   }
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
 }
 
 User.init(
@@ -43,18 +43,18 @@ User.init(
     },
   },
   {
-    // hooks: {
-    //   // set up beforeCreate lifecycle "hook" functionality
-    //   async beforeCreate(newUserData) {
-    //     newUserData.password = await bcrypt.hash(newUserData.password, 10);
-    //     return newUserData;
-    //   },
+    hooks: {
+      // set up beforeCreate lifecycle "hook" functionality
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
 
-    //   async beforeUpdate(updatedUserData) {
-    //     updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-    //     return updatedUserData;
-    //   }
-    // },
+      // async beforeUpdate(updatedUserData) {
+      //   updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+      //   return updatedUserData;
+      // }
+    },
     // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
     sequelize,
     timestamps: false,
--- public/assets/js/page1.js
+++ public/assets/js/page1.js
@@ -1,33 +1,3 @@
-// const loginform = document.getElementById("login-form");
-
-// function submitLogin(e) {
-//     e.preventDefault();
-//     const username = document.getElementById("username").value.trim()
-//     const password = document.getElementById("password").value.trim()
-
-//    if(username&&password){
-//        fetch('/api/users', {
-//            method: "post",
-//            body: JSON.stringify({
-//                username,
-//                password
-//            }),
-//            headers: {
-//                 "Content-Type": "application/json"
-//            }
-//        }).then((response) => {
-//            console.log(response);
-//            document.location.replace("/page2")
-//        }).catch((error) => {
-//            alert(error) 
-//            console.log(error)
-//        })
-//    };
-// };
-
-// loginform.addEventListener('submit', submitLogin);
-
-
 //   LOGIN 
 async function loginFormHandler(event) {
     event.preventDefault();
