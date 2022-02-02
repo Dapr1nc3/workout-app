d93d03b8bf2b88d98dfe06c4144932db078c7488
Alex Betancourt <alex@Alexs-MacBook-Pro.local>
2022-02-02 17:55:31 -0500

    get local storage to be persistent for each user

--- public/assets/js/page2.js
+++ public/assets/js/page2.js
@@ -1,19 +1,17 @@
 $(document).ready(function () {});
-let userResults = {}
 const username = localStorage.getItem("current_user")
 const savedData = JSON.parse(localStorage.getItem((username)))
+let userResults = savedData;
 
+// event listener/function to keep updating local storage with key inputs from user
 const planner = document.getElementById('planner');
 planner.addEventListener("keyup",(event) => {
   const {id, value} = event.target
   userResults = { ...userResults, [id]: value}
-  console.log(userResults)
   localStorage.setItem(username, JSON.stringify(userResults))
-  
 });
-console.log(savedData);
-console.log(savedData.textArea);
-//  so the text in the content can be displayed
+
+//  selects each text area from the front end by its id
 let textArea = document.querySelector("#textArea");
 let textArea2 = document.querySelector("#textArea2");
 let textArea3 = document.querySelector("#textArea3");
@@ -29,7 +27,7 @@ let textArea12 = document.querySelector("#textArea12");
 let textArea13 = document.querySelector("#textArea13");
 let textArea14 = document.querySelector("#textArea14");
 
-// saving them to local storage
+// makes selected text area value = what is stored in local storage for that user
 textArea.innerHTML = savedData.textArea
 textArea2.innerHTML = savedData.textArea2;
 textArea3.innerHTML = savedData.textArea3;
@@ -52,43 +50,31 @@ var dateDisplay = document.getElementById("currentDay");
 dateDisplay.innerHTML = show;
 console.log(dateDisplay);
 
-$(".saveBtn").on("click", function () {
-  var enterText = $(this).siblings(".description").val();
-  var hour = $(this).parent().attr("id");
-  localStorage.setItem(hour, enterText);
-});
+// prior code for local storage 
+// $(".saveBtn").on("click", function () {
+//   var enterText = $(this).siblings(".description").val();
+//   var hour = $(this).parent().attr("id");
+//   localStorage.setItem(hour, enterText);
+// });
+// var storedInput = localStorage.getItem("description");
+// $("#monday . description").val(localStorage.getItem("monday"));
+// $("#tuesday . description").val(localStorage.getItem("tuesday"));
+// $("#wednesday . description").val(localStorage.getItem("wednesday"));
+// $("#thursday . description").val(localStorage.getItem("thursday"));
+// $("#friday . description").val(localStorage.getItem("friday"));
+// $("#saturday . description").val(localStorage.getItem("saturday"));
+// $("#sunday . description").val(localStorage.getItem("sunday"));
+// $("#height . description").val(localStorage.getItem("height"));
+// $("#weight . description").val(localStorage.getItem("weight"));
+// $("#fat . description").val(localStorage.getItem("fat"));
+// $("#bmass . description").val(localStorage.getItem("bmass"));
+// $("#goal-week . description").val(localStorage.getItem("goal-week"));
+// $("#prev-goal . description").val(localStorage.getItem("prev-goal"));
+// $("#chall-goal . description").val(localStorage.getItem("chall-goal"));
 
-var storedInput = localStorage.getItem("description");
-
-$("#monday . description").val(localStorage.getItem("monday"));
-
-$("#tuesday . description").val(localStorage.getItem("tuesday"));
-
-$("#wednesday . description").val(localStorage.getItem("wednesday"));
-
-$("#thursday . description").val(localStorage.getItem("thursday"));
-
-$("#friday . description").val(localStorage.getItem("friday"));
-
-$("#saturday . description").val(localStorage.getItem("saturday"));
-
-$("#sunday . description").val(localStorage.getItem("sunday"));
-
-$("#height . description").val(localStorage.getItem("height"));
-
-$("#weight . description").val(localStorage.getItem("weight"));
-
-$("#fat . description").val(localStorage.getItem("fat"));
-
-$("#bmass . description").val(localStorage.getItem("bmass"));
-
-$("#goal-week . description").val(localStorage.getItem("goal-week"));
-
-$("#prev-goal . description").val(localStorage.getItem("prev-goal"));
-
-$("#chall-goal . description").val(localStorage.getItem("chall-goal"));
 
 async function logout() {
+  // function/call to logout the user and destroy the current session
   const response = await fetch('/api/users/logout', {
     method: 'post',
     headers: { 'Content-Type': 'application/json' }
